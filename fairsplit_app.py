0cf6e2adcbf97acbb228cd06564cf8c18b53ab4d
gryan1414 <gerard.ryan34@mail.dcu.ie>
2025-04-07 19:11:56 +0100

    Update fairsplit_app.py

--- fairsplit_app.py
+++ fairsplit_app.py
@@ -92,7 +92,7 @@ for idx, row in df.iterrows():
 st.subheader("📈 15-Day Energy Forecast for Room A")
 
 # Load CSV data (if not already loaded earlier in the script)
-df = pd.read_csv("sample_data/6-Month_Updated_Room_Energy_Usage_Data (1).csv")
+df = pd.read_csv("room_energy_data.csv")
 
 # Convert date
 df['date'] = pd.to_datetime(df['date'], dayfirst=True)
